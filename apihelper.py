4dbb90a9a3c87b29225da8b21577e37034d24910
Scott Moonen <smoonen@us.ibm.com>
2019-04-26 13:55:16 -0400

    Initial commit

---
+++ apihelper.py
@@ -0,0 +1,38 @@
+# Issue various VCS GET API calls, printing results and timings
+# Compatible with Python2 and Python3
+# Requires "requests" module:
+#   py2: sudo pip install requests
+#   py3: sudo python3 -m pip install requests
+
+import apikey
+import requests, sys, timeit
+
+# Helper for checking response code
+def check_results(resp, uri) :
+  if resp.status_code != 200 :
+    print("Error %d invoking %s" % (resp.status_code, uri))
+    sys.exit(1)
+# Helper for timing and checking a request
+def timed_json_request(uri, headers) :
+  start = timeit.default_timer()
+  resp = requests.get(uri, headers = headers)
+  end = timeit.default_timer()
+  check_results(resp, uri)
+  print('Elapsed: %f' % (end - start))
+  return resp.json()
+
+# Authenticate; returning header dictionary
+def authenticate() :
+  # Exchange API key for access and refresh token; populate header dictionary with this
+  headers = { 'Accept' : 'application/json' }
+  params = {
+    'grant_type' : 'urn:ibm:params:oauth:grant-type:apikey',
+    'apikey'     : apikey.APIKEY,
+  }
+  resp = requests.post('https://iam.bluemix.net/identity/token', data = params, headers = headers)
+  check_results(resp, 'https://iam.bluemix.net/identity/token')
+  json = resp.json()
+  headers['Authorization'] = 'Bearer %s' % json['access_token']
+  headers['X-Auth-Refresh-Token'] = json['refresh_token']
+  return headers
+
---
+++ get-tests.py
@@ -0,0 +1,38 @@
+# Issue various VCS GET API calls, printing results and timings
+# Compatible with Python2 and Python3
+# Requires "requests" module:
+#   py2: sudo pip install requests
+#   py3: sudo python3 -m pip install requests
+
+from __future__ import print_function
+import apihelper
+import pprint, sys
+
+# Authenticate with IBM Cloud using API key
+headers = apihelper.authenticate()
+
+# Get vcenters
+print('vCenters')
+json = apihelper.timed_json_request('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters', headers)
+pprint.pprint(json); print()
+if len(json) == 0 :
+  # No vCenters in account
+  sys.exit(0)
+vcenter_id = json[0]['id']
+
+print('vCenter[0] details')
+json = apihelper.timed_json_request('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/%s' % vcenter_id, headers)
+pprint.pprint(json); print()
+
+print('vCenter[0] clusters')
+json = apihelper.timed_json_request('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/%s/clusters' % vcenter_id, headers)
+pprint.pprint(json); print()
+if len(json) == 0 :
+  # No clusters in instance
+  sys.exit(0)
+cluster_id = json[0]['id']
+
+print('vcenter[0] clusters[0]')
+json = apihelper.timed_json_request('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/%s/clusters/%s' % (vcenter_id, cluster_id), headers)
+pprint.pprint(json); print()
+
