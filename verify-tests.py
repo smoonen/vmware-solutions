a381a4bb2b7108e3817b4016382654185401630f
Scott Moonen <smoonen@us.ibm.com>
2022-10-04 13:20:04 -0400

    Updates to: order NSXT, use v2 APIs, perform price checks

--- verify-tests.py
+++ verify-tests.py
@@ -3,110 +3,142 @@
 
 from __future__ import print_function
 import apihelper
-import pprint, random, sys
+import pprint, random, sys, uuid
 
 # Authenticate with IBM Cloud using API key
 headers = apihelper.authenticate()
 
 # Get supported DCs
-print('Supported data centers')
-data_centers = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v1/locations', headers)
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Supported data centers (%s)' % headers['x-global-transaction-id'])
+data_centers = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v2/locations', headers)
 pprint.pprint(data_centers); print()
+data_centers = data_centers['locations']
 
 # Get supported CPUs
-print('Supported CPU types')
-cpus = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v1/server_types', headers)
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Supported CPU types (%s)' % headers['x-global-transaction-id'])
+cpus = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v2/server_types', headers)
 pprint.pprint(cpus); print()
+cpus = cpus['server_types']
 
 # Get supported RAM
-print('Supported RAM types')
-ram = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v1/ram_types', headers)
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Supported RAM types (%s)' % headers['x-global-transaction-id'])
+ram = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v2/ram_types', headers)
 pprint.pprint(ram); print()
+ram = ram['ram_types']
 
 # Get supported disks
-print('Spuported disk types')
-disks = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v1/disk_types', headers)
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Spuported disk types (%s)' % headers['x-global-transaction-id'])
+disks = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v2/disk_types', headers)
 pprint.pprint(disks); print()
+disks = disks['disk_types']
 
 # Get supported NFS
-print('Supported NFS tiers')
-nfs = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v1/shared_storage_tiers', headers)
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Supported NFS tiers (%s)' % headers['x-global-transaction-id'])
+nfs = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v2/shared_storage_tiers', headers)
 pprint.pprint(nfs); print()
+nfs = nfs['shared_storage_tiers']
 
 # Get supported vSphere versions
-print('Supported vSphere versions')
-vers = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v1/vsphere_versions', headers)
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Supported vSphere versions (%s)' % headers['x-global-transaction-id'])
+vers = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v2/vsphere_versions', headers)
 pprint.pprint(vers); print()
+vers = vers['vsphere_versions']
 
 # Perform an order verification for an NFS instance
-print('Verify NFS instance')
-# Preselect server and ensure we choose RAM appropriate for that CPU type
-# NOTE: With the increase in bill-of-materials variations, there is a significant chance that the
-# configuration we settle on will result in an error.
-server = random.choice(cpus)['id']
-server_ram = random.choice([x for x in ram if server in x['supported_server_types']])['id']
-# Preselect Endurance tier
-shared_storage = random.choice(nfs)
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Verify NFS instance (%s)' % headers['x-global-transaction-id'])
+# Preselect server, RAM, and storage
+server = 'INTEL_INTEL_XEON_5218_2_30'
+assert(server in [cpu['id'] for cpu in cpus])
+server_ram = 'RAM_192_GB_DDR4_2133_ECC_REG'
+assert(server_ram in [x['id'] for x in ram])
+shared_storage = [x for x in nfs if x['size'] == 'STORAGE_SPACE_FOR_4_IOPS_PER_GB'][0]
+
 # Build order
 instance_request = {
   'name'            : 'test01',
-  'subdomain'       : 'test01',
-  'root_domain'     : 'example.com',
-  'location'        : random.choice(data_centers)['id'],
-  'vsphere_version' : random.choice(vers)['version'],
+  'host_prefix'     : 'host',
+  'root_domain'     : 'test1.example.com',
+  'vsphere_version' : '7.0',
   'dns_type'        : 'vsi',
   'domain_type'     : 'primary',
-  'hardware'        : {
-      'quantity'            : 2,
+  'vcs_type'        : 'vcs_nsx_t',
+  'management'      : {
+      'cluster_name'        : 'cluster01',
+      'quantity'            : 3,
+      'location'            : random.choice(data_centers)['id'],
+      'networking'          : { 'private_only' : False, },
       'customized_hardware' : {
-          'server'           : server,
-          'ram'              : server_ram,
-          'disks'            : [],
-          'vsan_cache_disks' : []
+          'server'      : server,
+          'ram'         : server_ram,
+          'disk_groups' : [ ],
         },
+      'shared_storages' : [ {
+          'iops'     : shared_storage['iops'],
+          'quantity' : 2,
+          'size'     : shared_storage['size'],
+          'volume'   : 4000,
+        } ],
     },
-  'network'         : {
-      'private_only' : False,
-    },
-  'shared_storages' : [ {
-      'iops'     : shared_storage['iops'],
-      'quantity' : 2,
-      'size'     : shared_storage['size'],
-      'volume'   : 4000,
-    } ],
+  'license_keys' : { 'nsx' : { 'license_type' : 'dc_professional' } },
 }
-result = apihelper.timed_json_post('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters?verify_only=true', instance_request, headers)
+result = apihelper.timed_json_post('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/nsxt?verify_only=true&check_price=true', instance_request, headers)
 pprint.pprint(result); print()
 
 # If there is a VCS instance in our account, perform an add cluster verification as well
-print('Get vCenter instances')
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Get vCenter instances (%s)' % headers['x-global-transaction-id'])
 vcenters = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters', headers)
 if len(vcenters) == 0 :
   sys.exit(0)
+# Get instance cluster detail
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Get network details for first existing cluster (%s)' % headers['x-global-transaction-id'])
+vcenter_cluster0_networks = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/%s/clusters/%s/vlans' % (vcenters[0]['id'], 0), headers)
 # Build order
-print('Verify NFS cluster')
-cluster_request = instance_request.copy()
-del cluster_request['name']
-del cluster_request['subdomain']
-del cluster_request['root_domain']
-del cluster_request['vsphere_version']
-del cluster_request['dns_type']
-del cluster_request['domain_type']
-cluster_request['cluster_name']   = 'cluster02'
-cluster_request['is_default_pod'] = True
-result = apihelper.timed_json_post('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/%s/clusters?verify_only=true' % vcenters[0]['id'], cluster_request, headers)
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Verify NFS cluster (%s)' % headers['x-global-transaction-id'])
+private_vlan            = [x for x in vcenter_cluster0_networks if x['id'] == 'private_vlan'][0]['number']
+additional_private_vlan = [x for x in vcenter_cluster0_networks if x['id'] == 'additional_private_vlan'][0]['number']
+public_vlan             = [x for x in vcenter_cluster0_networks if x['id'] == 'public_vlan'][0]['number']
+cluster_request = {
+  'cluster_name'    : 'cluster02',
+  'location'        : vcenters[0]['location'],
+  'host_prefix'     : 'host',
+  'license_keys'    : { },
+  'network'         : { 'private_only' : False },
+  'networking'      : {
+      'private_vlan'            : { 'num': str(private_vlan) },
+      'additional_private_vlan' : { 'num': str(additional_private_vlan) },
+      'public_vlan'             : { 'num': str(public_vlan) },
+  },
+  'hardware'        : {
+      'quantity'            : 2,
+      'customized_hardware' : instance_request['management']['customized_hardware'],
+  },
+  'shared_storages' : instance_request['management']['shared_storages'],
+}
+result = apihelper.timed_json_post('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/%s/clusters?verify_only=true&check_price=true' % vcenters[0]['id'], cluster_request, headers)
 pprint.pprint(result); print()
 
 # Perform an add host verification
-print('Get clusters')
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Get clusters (%s)' % headers['x-global-transaction-id'])
 clusters = apihelper.timed_json_get('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/%s/clusters' % vcenters[0]['id'], headers)
 if len(clusters) == 0 :
   sys.exit(0)
-print('Verify add host')
+headers['x-global-transaction-id'] = str(uuid.uuid4())
+print('Verify add host (%s)' % headers['x-global-transaction-id'])
 addhost_request = {
   'quantity'         : 1,
   'maintenance_mode' : True,
 }
-result = apihelper.timed_json_post('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/%s/clusters/%s/hosts?verify_only=true' % (vcenters[0]['id'], clusters[0]['id']), addhost_request, headers)
+result = apihelper.timed_json_post('https://api.vmware-solutions.cloud.ibm.com/v1/vcenters/%s/clusters/%s/hosts?verify_only=true&check_price=true' % (vcenters[0]['id'], clusters[0]['id']), addhost_request, headers)
 pprint.pprint(result); print()
 
